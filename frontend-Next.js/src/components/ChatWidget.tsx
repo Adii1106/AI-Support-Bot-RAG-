e3a641879199bdc7df42f0343e4ffe96cf3f6745
Aditya Mishra <aditya.1165.mishra@gmail.com>
2026-03-10 11:00:00 +0530

    initial chat widget UI draft

---
+++ frontend-Next.js/src/components/ChatWidget.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ChatWidget() {
+  return (
+    <div className='fixed bottom-4 right-4'>
+      <div className='bg-indigo-600 text-white p-4 rounded-full'>Chat</div>
+    </div>
+  );
+}
