8e9dc1446c60bccc8ccc728b1caad2740e356026
Aditya Mishra <aditya.1165.mishra@gmail.com>
2026-03-11 16:00:00 +0530

    full chat widget implementation with stream handling

--- frontend-Next.js/src/components/ChatWidget.tsx
+++ frontend-Next.js/src/components/ChatWidget.tsx
@@ -1,9 +1,1 @@
-import React from 'react';
-
-export default function ChatWidget() {
-  return (
-    <div className='fixed bottom-4 right-4'>
-      <div className='bg-indigo-600 text-white p-4 rounded-full'>Chat</div>
-    </div>
-  );
-}
+export default function ChatWidget() { return <div>Simple Chat</div>; }
